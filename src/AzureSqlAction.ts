a319818f297a0af38077b68684002f9287459015
XLipcak <jakublipcak@gmail.com>
2020-10-20 07:47:04 +0200

    Git test 11.

--- src/AzureSqlAction.ts
+++ src/AzureSqlAction.ts
@@ -71,6 +71,7 @@ export default class AzureSqlAction {
         let fileObjs = fs.readdirSync(/*inputs.sqlFolder*/ './Objects/sql_action_test', { withFileTypes: true });
 
         fileObjs.forEach(file => {
+            console.log(JSON.stringify(file));
             exec.exec(`"${sqlCmdPath}" -S ${inputs.serverName} -d ${inputs.connectionString.database} -U "${inputs.connectionString.userId}" -P "${inputs.connectionString.password}" -i ${file.toString()} ${inputs.additionalArguments}`);
         });
 
